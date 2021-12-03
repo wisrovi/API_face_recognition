eb7be6deffb121531c9978b1ace5a77f8982df3a
William Rodriguez <william.rodriguez@smartiglobal.com.co>
2021-12-03 10:19:58 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='API_face_recognition',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='wisrovi',
+    author_email='wisrovi.rodriguez@gmail.com',
+    description='Api reconocimiento facial'
+)
