c3e0f6db7c2678e7f479ea6a329cce693ed65d8c
WISROVI <wisrovi.rodriguez@gmail.com>
2023-08-12 20:55:51 +0200

    [FEATURE] unit test with pytest for api and project

---
+++ src/test/_test_tf_compare_faces.py
@@ -0,0 +1,57 @@
+import warnings
+
+import numpy as np
+
+from shared.tf_compare_faces.KerasFace import KerasFace
+from shared.tf_compare_faces.MatchFace import MatchFace
+
+warnings.filterwarnings("ignore")
+
+match_face = MatchFace(euclidean_umbral=100, cosine_umbral=0.4)
+
+
+class TestTf_compare_faces:
+    def test_emb_and_emb(self):
+        emb1 = KerasFace("/app/test/resources/u/robert_1.jpg").embedding
+        emb2 = KerasFace("/app/test/resources/robert_2.jpg").embedding
+        d = match_face.compare(emb1, emb2)
+
+        assert isinstance(d, tuple)
+        assert isinstance(d[0], tuple)
+        assert isinstance(d[1], tuple)
+        assert isinstance(d[0][0], np.float32) or \
+            isinstance(d[0][0], np.float64)
+        assert isinstance(d[1][0], np.float32) or \
+            isinstance(d[1][0], np.float64)
+        assert d[0][1]
+        assert d[1][1]
+
+    def test_emb_and_path(self):
+        emb1 = KerasFace("/app/test/resources/u/robert_1.jpg").embedding
+        d = match_face.compare(emb1, "/app/test/resources/robert_2.jpg")
+
+        assert isinstance(d, tuple)
+        assert isinstance(d[0], tuple)
+        assert isinstance(d[1], tuple)
+        assert isinstance(d[0][0], np.float32) or \
+            isinstance(d[0][0], np.float64)
+        assert isinstance(d[1][0], np.float32) or \
+            isinstance(d[1][0], np.float64)
+        assert d[0][1]
+        assert d[1][1]
+
+    def test_path_and_path(self):
+        d = match_face.compare(
+            "/app/test/resources/u/robert_1.jpg",
+            "/app/test/resources/robert_2.jpg",
+        )
+
+        assert isinstance(d, tuple)
+        assert isinstance(d[0], tuple)
+        assert isinstance(d[1], tuple)
+        assert isinstance(d[0][0], np.float32) or \
+            isinstance(d[0][0], np.float64)
+        assert isinstance(d[1][0], np.float32) or \
+            isinstance(d[1][0], np.float64)
+        assert d[0][1]
+        assert d[1][1]
--- src/test/test_facecode_aes.py
+++ src/test/test_facecode_aes.py
@@ -1,3 +1,4 @@
+import os
 from shared.FaceRecognition.Facecode_AES import Facecode_AES
 from shared.Image import Image
 
@@ -7,21 +8,19 @@ image = Image()
 
 # save using pickle
 face_code = Facecode_AES(max_distance=0.9)
+os.makedirs("/models/face_recognion", exist_ok=True)
 with open("/models/face_recognion/face_code.pkl", "wb") as f:
     pickle.dump(face_code, f)
 
 
 class TestFaceCodeAES:
-
     def test_facecode_pillow_and_path(self):
         # load using pickle
         with open("/models/face_recognion/face_code.pkl", "rb") as f:
             face_code = pickle.load(f)
         face_code.distance = 0.9
 
-        buffer_image = image.read_image_using_pillow(
-            "/test/face_recognition/u/robert_1.jpg"
-        )
+        buffer_image = image.read_image_using_pillow("test/resources/u/robert_1.jpg")
         face_code.path = buffer_image
 
         person_1 = {"first_name": "Robert", "vector": face_code.fingerprint}
@@ -29,7 +28,7 @@ class TestFaceCodeAES:
         all_vectors = [person_1["vector"]]
         all_names = [person_1["first_name"]]
 
-        face_code.path = "/test/face_recognition/robert_2.jpg"
+        face_code.path = "test/resources/robert_2.jpg"
         result = face_code.compare_fingerprints(all_vectors, all_names)
         assert face_code.vector.any()
         assert result[0] == person_1["first_name"]
@@ -44,7 +43,7 @@ class TestFaceCodeAES:
 
         # find fingerprint or image and put in buffer on face_code.path
         buffer_image_know = image.read_image_using_pillow(
-            "/test/face_recognition/u/robert_1.jpg"
+            "test/resources/u/robert_1.jpg"
         )
         face_code.path = buffer_image_know
 
@@ -58,7 +57,7 @@ class TestFaceCodeAES:
 
         # find fingerprint or unknown image and put in buffer on face_code.path
         buffer_image_unknown = image.read_image_using_pillow(
-            "/test/face_recognition/robert_2.jpg"
+            "test/resources/robert_2.jpg"
         )
         face_code.path = buffer_image_unknown
 
@@ -71,11 +70,8 @@ class TestFaceCodeAES:
         assert isinstance(face_code.fingerprint, str)
 
     def test_facecode_opencv_and_opencv(self):
-
         # load image using opencv and put in buffer
-        face_code.path = image.read_image_using_opencv(
-            "/test/face_recognition/u/robert_1.jpg"
-        )
+        face_code.path = image.read_image_using_opencv("test/resources/u/robert_1.jpg")
 
         # create a database with all fingerprints and names of people
         person_know = {
@@ -86,9 +82,7 @@ class TestFaceCodeAES:
         all_names = [person_know["first_name"]]
 
         # load image using opencv and put in buffer
-        face_code.path = image.read_image_using_opencv(
-            "/test/face_recognition/robert_2.jpg"
-        )
+        face_code.path = image.read_image_using_opencv("test/resources/robert_2.jpg")
 
         # compare the unknown fingerprint with the database
         result = face_code.compare_fingerprints(all_vectors, all_names)
@@ -104,13 +98,13 @@ class TestFaceCodeAES:
             face_code = pickle.load(f)
         face_code.distance = 0.9
 
-        face_code.path = "/test/face_recognition/u/robert_1.jpg"
+        face_code.path = "test/resources/u/robert_1.jpg"
 
         person_1 = {"first_name": "Robert", "vector": face_code.fingerprint}
         all_vectors = [person_1["vector"]]
         all_names = [person_1["first_name"]]
 
-        face_code.path = "/test/face_recognition/robert_2.jpg"
+        face_code.path = "test/resources/robert_2.jpg"
         result = face_code.compare_fingerprints(all_vectors, all_names)
         assert face_code.vector.any()
         assert result[0] == person_1["first_name"]
